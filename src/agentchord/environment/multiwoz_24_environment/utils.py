4c14f55c49565433cba7e579a8afa1a442a50f00
yxc_cyber <yxc_yxc@yeah.net>
2025-03-11 16:15:10 -0500

    Format the goals.json

--- src/agentchord/environment/multiwoz_24_environment/utils.py
+++ src/agentchord/environment/multiwoz_24_environment/utils.py
@@ -173,7 +173,7 @@ def insertSpace(token, text):
         sidx = text.find(token, sidx)
         if sidx == -1:
             break
-        if sidx + 1 < len(text) and re.match("[0-9]", text[sidx - 1]) and re.match("[0-9]", text[sidx + 1]):
+        if sidx + 1 < len(text) and re.match(r"[0-9]", text[sidx - 1]) and re.match(r"[0-9]", text[sidx + 1]):
             sidx += 1
             continue
         if text[sidx - 1] != " ":
@@ -198,7 +198,7 @@ def normalize(text):
     text = re.sub(r"b and b", "bed and breakfast", text)
 
     # normalize phone number
-    ms = re.findall("\(?(\d{3})\)?[-.\s]?(\d{3})[-.\s]?(\d{4,5})", text)
+    ms = re.findall(r"\(?(\d{3})\)?[-.\s]?(\d{3})[-.\s]?(\d{4,5})", text)
     if ms:
         sidx = 0
         for m in ms:
@@ -209,46 +209,46 @@ def normalize(text):
             text = text.replace(text[sidx:eidx], "".join(m))
 
     # normalize postcode
-    ms = re.findall("([a-z]{1}[\. ]?[a-z]{1}[\. ]?\d{1,2}[, ]+\d{1}[\. ]?[a-z]{1}[\. ]?[a-z]{1}|[a-z]{2}\d{2}[a-z]{2})",
+    ms = re.findall(r"([a-z]{1}[\. ]?[a-z]{1}[\. ]?\d{1,2}[, ]+\d{1}[\. ]?[a-z]{1}[\. ]?[a-z]{1}|[a-z]{2}\d{2}[a-z]{2})",
                     text)
     if ms:
         sidx = 0
         for m in ms:
             sidx = text.find(m, sidx)
             eidx = sidx + len(m)
-            text = text[:sidx] + re.sub("[,\. ]", "", m) + text[eidx:]
+            text = text[:sidx] + re.sub(r"[,\. ]", "", m) + text[eidx:]
 
     # weird unicode bug
     text = re.sub(u"(\u2018|\u2019)", "'", text)
 
     # replace time and and price
-    timepat = re.compile("\d{1,2}[:]\d{1,2}")
-    pricepat = re.compile("\d{1,3}[.]\d{1,2}")
+    timepat = re.compile(r"\d{1,2}[:]\d{1,2}")
+    pricepat = re.compile(r"\d{1,3}[.]\d{1,2}")
     text = re.sub(timepat, " [value_time] ", text)
     text = re.sub(pricepat, " [value_price] ", text)
     #text = re.sub(pricepat2, "[value_price]", text)
 
     # replace st.
     text = text.replace(";", ",")
-    text = re.sub("$\/", "", text)
+    text = re.sub(r"$\/", "", text)
     text = text.replace("/", " and ")
 
     # replace other special characters
     text = text.replace("-", " ")
-    text = re.sub("[\":\<>@\(\)]", "", text)
+    text = re.sub(r"[\":\<>@\(\)]", "", text)
 
     # insert white space before and after tokens:
     for token in ["?", ".", ",", "!"]:
         text = insertSpace(token, text)
 
-    # insert white space for "s
-    text = insertSpace("\"s", text)
+    # insert white space for 's
+    text = insertSpace("\'s", text)
 
-    # replace it"s, does"t, you"d ... etc
-    text = re.sub("^\"", "", text)
-    text = re.sub("\"$", "", text)
-    text = re.sub("\"\s", " ", text)
-    text = re.sub("\s\"", " ", text)
+    # replace it's, does't, you'd ... etc
+    text = re.sub(r"^\'", "", text)
+    text = re.sub(r"\'$", "", text)
+    text = re.sub(r"\'\s", " ", text)
+    text = re.sub(r"\s\'", " ", text)
     with open(os.path.join(ENV_PATH, "mapping.pair")) as fin:
         replacements = []
         for line in fin.readlines():
@@ -259,14 +259,14 @@ def normalize(text):
         text = text.replace(fromx, tox)[1:-1]
 
     # remove multiple spaces
-    text = re.sub(" +", " ", text)
+    text = re.sub(r" +", " ", text)
 
     # concatenate numbers
     tmp = text
     tokens = text.split()
     i = 1
     while i < len(tokens):
-        if re.match(u"^\d+$", tokens[i]) and re.match(u"\d+$", tokens[i - 1]):
+        if re.match(r"^\d+$", tokens[i]) and re.match(r"\d+$", tokens[i - 1]):
             tokens[i - 1] += tokens[i]
             del tokens[i]
         else:
